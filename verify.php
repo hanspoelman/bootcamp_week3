687507aa9064c51b8d248ed775714271ec319716
hanspoelman <44786293+hanspoelman@users.noreply.github.com>
2018-11-23 12:29:31 +0100

    Delete verify.php

--- verify.php
+++
@@ -1,8 +0,0 @@
-<?php
-                    session_start();
-                    if ($_SESSION["code"] == $_POST["captchascript.php"]) {
-                        echo "succesvol verzonden";
-                    } else {
-                        die("fouttieve invoer");
-                    }
-                    ?>
